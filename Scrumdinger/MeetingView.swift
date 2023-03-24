15aed993605c9c812cfe7ba76e71fa820eb1d90c
Anton <antony.adamson@gmail.com>
2023-03-24 16:48:51 +0600

    Секция 3 закончена

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Антон Адамсон on 24.03.2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Прошло Секунд")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Секунд Осталось")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Участник 1 из 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
