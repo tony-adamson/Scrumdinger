e8ce594f03cfefbe5e99ebf49d9cc8fca0ad7efc
Anton <antony.adamson@gmail.com>
2023-03-25 00:57:22 +0600

    Создано Card View

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -25,14 +25,14 @@ enum Theme: String {
     case teal
     case yellow
     
-    var color: Color {
+    var accentColor: Color {
         switch self {
         case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
-    
     var mainColor: Color {
         Color(rawValue)
     }
 }
+
