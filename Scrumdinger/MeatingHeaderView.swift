ab672c2eafb6f800666707c1f83e1c551fffadd7
Anton <antony.adamson@gmail.com>
2023-03-25 18:34:43 +0600

    Промежуточный коммит перед разделением митинг вью Так же были добавлены переходы между сценами и возможность изменения цвета встречи

---
+++ Scrumdinger/MeatingHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  MeatingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Антон Адамсон on 25.03.2023.
+//
+
+import SwiftUI
+
+struct MeatingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeatingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeatingHeaderView()
+    }
+}
