8dadf5dcb5fe5550f4f39cb97d451f2ffa901248
anton.a <anton.adamson@gmail.com>
2024-04-19 22:46:08 +0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Антон Адамсон on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
