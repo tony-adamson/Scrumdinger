5f03ae892ce1950fa5abdb87b8e73706fbf4d70e
anton.a <anton.adamson@gmail.com>
2024-04-21 11:53:54 +0600

    Drawing the timer view

--- Scrumdinger/View/MeetingView.swift
+++ Scrumdinger/View/MeetingView.swift
@@ -38,9 +38,10 @@ struct MeetingView: View {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
                                   secondsRemaining: scrumTimer.secondsRemaining,
                                   theme: scrum.theme)
-                Circle()
-                    .strokeBorder(lineWidth: 24)
-                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+                MeetingTimerView(speakers: scrumTimer.speakers,
+                                 theme: scrum.theme)
+                MeetingFooterView(speakers: scrumTimer.speakers, 
+                                  skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
